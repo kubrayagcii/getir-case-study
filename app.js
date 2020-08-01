6e989c48bfe69b4c8da388421cb9ec996ace56d9
unknown <you@example.com>
2020-08-01 18:56:47 +0300

    commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({ extended: true }));
 const connectDatabase = require('./config/database')
 
 //Setting up config.env file variables
--- routes/posts.js
+++ routes/posts.js
@@ -5,7 +5,9 @@ const bodyParser = require('body-parser');
 const Activity = require('../models/model');
 
 
-router.get('/',async (req,res) => {
+
+
+/*router.get('/',async (req,res) => {
     
     var pipeline = [
         
@@ -36,6 +38,50 @@ router.get('/',async (req,res) => {
     }
 
 
+});*/
+
+
+
+router.post('/',async (req,res) => {
+
+    router.use( bodyParser.json() );  
+    var payload={
+        "startDate": "2016-01-26",
+        "endDate": "2018-02-02",
+        "minCount": 2700,
+        "maxCount": 3000
+        }
+    
+    
+    req.body = payload
+
+    var pipeline = [
+        
+        { $match : {createdAt : {"$gte": new Date(req.body.startDate),"$lte": new Date(req.body.endDate)}}},
+        { $project: {
+            _id: 0,
+            key : 1,
+            createdAt :1,
+            totalCount: { $sum: "$counts" }
+
+        }},
+        { $match : {totalCount: {"$gte":req.body.minCount,"$lte":req.body.maxCount}}},
+           
+    ];
+     
+    const request = await Activity.aggregate(pipeline);
+    if(res.status(200)){
+        res.status(200).json({
+            code: 0,
+            message : 'Success',
+            records : request
+        });
+    }else{
+        res.json({
+            code:1,
+            message:'Fail'
+        })
+    }
 });
 
 module.exports =  router ;
